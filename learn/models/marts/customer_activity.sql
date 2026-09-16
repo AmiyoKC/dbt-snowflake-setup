e6d0964587ac701970d860f8c128eb8815348b94
AmiyoKC <amiyo.chatterjee@gmail.com>
2026-09-17 02:06:20 +0530

    Add customer_activity, an incremental table

---
+++ learn/models/marts/customer_activity.sql
@@ -0,0 +1,17 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='customer_id'
+    )
+}}
+
+select
+    customer_id,
+    customer_name,
+    email,
+    signup_date
+from {{ ref('stg_customers') }}
+
+{% if is_incremental() %}
+where signup_date > (select max(signup_date) from {{ this }})
+{% endif %}
